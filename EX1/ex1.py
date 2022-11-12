181791c539862d284a52a9fb53546bdb1a239509
yotami <yotam.inbar@mail.huji.ac.il>
2022-11-12 13:57:05 +0200

    Final

--- EX1/ex1.py
+++ EX1/ex1.py
@@ -1,230 +1,186 @@
 import spacy
 from datasets import load_dataset
-import csv
-import random
 import numpy as np
 
-
 nlp = spacy.load("en_core_web_sm")
-dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split="train")
+
+
+# dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split="train")
+
+
 # for text in dataset['text']:
 #     doc = nlp(text)
 
-class unigramModel():
+class UnigramModel:
+
     def __init__(self, dataset):
         self.freqDict = {}
-        self.freqDict.keys()
         self.dataset = dataset
+        self.numWords = 0
 
     def fit(self):
         for text in self.dataset['text']:
             doc = nlp(text)
             for token in doc:
                 if token.is_alpha:
+                    self.numWords += 1
                     self.freqDict[token.lemma_] = \
                         self.freqDict.get(token.lemma_, 0) + 1
-        self.words, self.weights = list(self.freqDict.keys()), list(
-            self.freqDict.values())
-        self.totalWords = sum(self.weights)
 
-    def predict(self):
-        r = random.choices(population=self.words, weights=self.weights, k=1)
-
-
-    def wordProbability(self, word):
+    def getWordProbability(self, lemma):
         try:
-            return self.freqDict[word] / self.totalWords
-        except KeyError:
-            return 0
+            return np.log(self.freqDict[lemma] / self.numWords)
+        except KeyError or ZeroDivisionError:
+            return -np.inf
 
-    def sentenceProbability(self, sent):
+    def getSentenceProbability(self, sent):
         doc = nlp(sent)
-        alphaTokens = ["START"] + [token.lemma_ for token in doc if
-                                   token.is_alpha]
-        logProb = None
-        for i, token in enumerate(alphaTokens):
-            try:
-                if logProb == None:
-                    logProb = self.freqDict[token] / self.totalWords
-                else:
-                    logProb *= self.freqDict[token] / self.totalWords
-            except KeyError:
-                return 0
+        alphaTokens = [token.lemma_ for token in doc if token.is_alpha]
 
-        if logProb == None:
-            return 0
+        logProb = 0
+        for lemma in alphaTokens:
+            logProb += self.getWordProbability(lemma)
+
         return logProb
 
-    # def measurePerplexity(se
+    def measurePerplexity(self, sents):
+        l = 0
+        test_words_num = 0
+        for sent in sents:
+            l += self.getSentenceProbability(sent)
+
+            doc = nlp(sent)
+            test_words_num += len([token.lemma_ for token in doc if token.is_alpha])
+
+        l /= test_words_num
+
+        return np.e ** (-l)
 
 
+class BigramModel:
 
-
-
-
-
-
-class bigramModel():
     def __init__(self, dataset):
         self.dataset = dataset
-        self.mat = None
-
-    def firstPass(self):
-        self.numberAssociation = {"START": 0}
-        latest_indx = 1
-
-        for text in self.dataset['text']:
-            doc = nlp(text)
-            for token in doc:
-                # check if it is alpha
-                if not token.is_alpha:
-                    continue
-                # check if it exists in numberAssociation
-                if token.lemma_ in self.numberAssociation.keys():
-                    continue
-                # else add to numberAssociation with latest_indx
-                self.numberAssociation[token.lemma_] = latest_indx
-                latest_indx += 1
+        self.wordNumberAssociation = {}
+        self.numDistinctWords = 0
+        self.numWords = 0
+        self.dictMat = {}
 
     def fit(self):
-        self.firstPass()
-        self.mat = {}
-        for key in self.numberAssociation.keys():
-            self.mat[key] = {key:0 for key in self.numberAssociation.keys()}
-
         for text in self.dataset['text']:
             doc = nlp(text)
             alphaTokens = ["START"] + [token.lemma_ for token in doc if token.is_alpha]
             for i, token in enumerate(alphaTokens):
                 if i == 0:
                     continue
-                prevIndx = self.numberAssociation[alphaTokens[i-1]]
-                currIndex = self.numberAssociation[alphaTokens[i]]
-                self.mat[alphaTokens[i-1]][alphaTokens[i]] += 1
 
-    def Q2(self, prevWord):
-        # predict next most likely word for prevWord
-        if prevWord not in self.numberAssociation.keys():
-            return
-        # prevIndex = self.numberAssociation[prevWord]
-        return max(self.mat[prevWord],key=self.mat[prevWord].get)
-        # nextIndex = np.argmax(self.mat[prevIndex][1:])
-        # numList = list(self.numberAssociation.keys())
-        # return numList[nextIndex + 1]
+                self.numWords += 1
+
+                if alphaTokens[i - 1] in self.dictMat:
+                    self.dictMat[alphaTokens[i - 1]][alphaTokens[i]] = \
+                        self.dictMat[alphaTokens[i - 1]].get(alphaTokens[i], 0) + 1
+                else:
+                    self.dictMat[alphaTokens[i - 1]] = {alphaTokens[i]: 1}
 
 
-    def twoWordProbability(self, firstWord, secondWord):
+    def getTwoWordProbability(self, firstLemma, secondLemma):
         try:
-            prevIndex = self.numberAssociation[firstWord]
-            currIndex = self.numberAssociation[secondWord]
-            return (self.mat[prevIndex][currIndex] /
-                             sum(self.mat[prevIndex].values()))
-        except KeyError:
-            return 0
+            return np.log(self.dictMat[firstLemma][secondLemma] / np.sum(list(self.dictMat[firstLemma].values())))
 
+        except KeyError or ZeroDivisionError:
+            return -np.inf
 
-    def sentenceProbability(self, sent):
+    def getSentenceProbability(self, sent):
         doc = nlp(sent)
-        alphaTokens = ["START"] + [token.lemma_ for token in doc if
-                                   token.is_alpha]
-        logProb = None
+        alphaTokens = ["START"] + [token.lemma_ for token in doc if token.is_alpha]
+        logProb = 0
         for i, token in enumerate(alphaTokens):
             if i == 0:
                 continue
-            try:
-                prevIndex = self.numberAssociation[alphaTokens[i-1]]
-                currIndex = self.numberAssociation[alphaTokens[i]]
-                if logProb:
-                    logProb *= (self.mat[alphaTokens[i-1]][alphaTokens[i]]
-                                  / sum(self.mat[alphaTokens[i-1]].values()))
-                else:
-                    logProb = (self.mat[alphaTokens[i-1]][alphaTokens[i]]
-                                  / sum(self.mat[alphaTokens[i-1]].values()))
+            logProb += self.getTwoWordProbability(alphaTokens[i - 1], alphaTokens[i])
 
-            except KeyError:
-                return 0
-        if logProb == None:
-            return 0
         return logProb
 
-    def measurePerplexity(self, testSet):
+    def measurePerplexity(self, sents):
+        l = 0
+        test_words_num = 0
+        for sent in sents:
+            l += self.getSentenceProbability(sent)
 
-        # get number of unique tokens in testSet
-        uniqueTokens = set()
-        for sent in testSet:
             doc = nlp(sent)
-            currTokens = set([token.lemma_ for token in doc if
-                                   token.is_alpha])
-            uniqueTokens.update(currTokens)
+            test_words_num += len([token.lemma_ for token in doc if token.is_alpha])
 
-        logTerm = 0
-        # get total logProb
-        for sent in testSet:
-            logTerm += np.log(self.sentenceProbability(sent))
+        l /= test_words_num
 
-        return np.e**(
-            -(logTerm / len(uniqueTokens)))
+        return np.e ** (-l)
 
 
-class secondDegreeInterpolation():
-    def __init__(self, testSet, dataset, lambda1, lambda2, bigram):
-        self.testSet = testSet
-        self.unigram = unigramModel(dataset)
-        self.unigram.fit()
+class SecondDegreeInterpolation:
+    def __init__(self, lambda1, lambda2, unigram, bigram):
+        self.unigram = unigram
         self.bigram = bigram
         self.lambda1 = lambda1
         self.lambda2 = lambda2
 
-
-    def sentProbability(self, sent, returnLog = True):
+    def getSentenceProbability(self, sent):
         doc = nlp(sent)
-        alphaTokens = ["START"] + [token.lemma_ for token in doc if
-                                   token.is_alpha]
-        sentProb = 1
+        alphaTokens = ["START"] + [token.lemma_ for token in doc if token.is_alpha]
+        logProb = 0
         for i, token in enumerate(alphaTokens):
             if i == 0:
                 continue
-            uni = self.unigram.wordProbability(token)
-            bi = self.bigram.twoWordProbability(alphaTokens[i-1],alphaTokens[i])
-            sentProb *= (self.lambda1 * uni + self.lambda2 * bi)
+            logProb += np.log(
+                self.lambda1 * np.exp(self.unigram.getWordProbability(alphaTokens[i])) +
+                self.lambda2 * np.exp(self.bigram.getTwoWordProbability(alphaTokens[i - 1], alphaTokens[i])))
 
-        if returnLog:
-            return np.log(sentProb)
+        return logProb
 
-        return sentProb
+    def measurePerplexity(self, sents):
+        l = 0
+        test_words_num = 0
+        for sent in sents:
+            l += self.getSentenceProbability(sent)
 
-
-    def computePerplexity(self):
-        uniqueTokens = set()
-        for sent in self.testSet:
             doc = nlp(sent)
-            currTokens = set([token.lemma_ for token in doc if
-                                   token.is_alpha])
-            uniqueTokens.update(currTokens)
+            test_words_num += len([token.lemma_ for token in doc if token.is_alpha])
 
-        logTerm = 0
-        # get total logProb
-        for sent in self.testSet:
-            logTerm += self.sentProbability(sent)
+        l /= test_words_num
 
-        return np.e**(
-            -(logTerm / len(uniqueTokens)))
+        return np.e ** (-l)
 
+
+def question2(bigramModel):
+    print("Question 2:")
+    # prevIndex = bigramModel.wordNumberAssociation["in"]
+    print("The most probable next word is: " +
+          max(bigramModel.dictMat["in"], key=bigramModel.dictMat["in"].get))
+
+
+def question3or4(model, q_num):
+    print("Question " + str(q_num) + ":")
+
+    sent1 = "Brad Pitt was born in Oklahoma"
+    sent2 = "The actor was born in USA"
+
+    print("probability of " + sent1 + ": " + str(model.getSentenceProbability(sent1)))
+    print("probability of " + sent2 + ": " + str(model.getSentenceProbability(sent2)))
+    print("Perplexity of both: " + str(model.measurePerplexity([sent1, sent2])))
 
 
 if __name__ == '__main__':
-    dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split="train")
-    s = bigramModel(dataset)
-    s.fit()
-    print("I have a house in, ML NEXT WORD:", s.Q2("in"))
-    sent1 = "Brad Pitt was born in Oklahoma"
-    sent2 = "The actor was born in USA"
-    sents = [sent1, sent2]
-    print("Brad Pitt was born in Oklahoma, SENT PROB: ", s.sentenceProbability(sent1))
-    print("The actor was born in USA, SENT PROB: ",  s.sentenceProbability(sent2))
-    print(" Q3 B, TEST SET PERPLEXITY: ", s.measurePerplexity(sents))
-    interpolation = secondDegreeInterpolation(sents, dataset, lambda1=1/3, lambda2=2/3, bigram=s)
-    print("Brad Pitt was born in Oklahoma, SENT PROB: ", interpolation.sentProbability(sent1, returnLog=False))
-    print("The actor was born in USA, SENT PROB: ",  interpolation.sentProbability(sent2, returnLog=False))
-    print("SENTS INTERPOLATION:", interpolation.computePerplexity())
+    # --------------- Question 1 ----------------
+
+    dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split="train")  # [0:500]
+
+    unigram = UnigramModel(dataset)
+    unigram.fit()
+
+    bigram = BigramModel(dataset)
+    bigram.fit()
+
+    interpolation = SecondDegreeInterpolation(1 / 3, 2 / 3, unigram, bigram)
+
+    question2(bigram)
+    question3or4(bigram, 3)
+    question3or4(interpolation, 4)
 
